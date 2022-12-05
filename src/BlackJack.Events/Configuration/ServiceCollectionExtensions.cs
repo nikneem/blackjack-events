d70f8bcb244f8ab239b464473c44e64cc5f8d14d
Eduard <e.keilholz@4dotnet.nl>
2022-12-05 16:13:59 +0100

    Adding DI

---
+++ src/BlackJack.Events/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using BlackJack.Events.Abstractions.Sender;
+using BlackJack.Events.Sender;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlackJack.Events.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBlackJackEvents(this IServiceCollection services)
+    {
+        services.AddTransient<IEventGridSenderFactory, EventGridSenderFactory>();
+        return services;
+    }
+}
