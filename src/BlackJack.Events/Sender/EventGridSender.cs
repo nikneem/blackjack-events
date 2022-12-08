11bc5459bdc8bfe09739f5c43c8f20d37eb29fd7
Eduard <e.keilholz@4dotnet.nl>
2022-12-08 15:20:38 +0100

    Change to cloud event schema

--- src/BlackJack.Events/Sender/EventGridSender.cs
+++ src/BlackJack.Events/Sender/EventGridSender.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Messaging.EventGrid;
+﻿using Azure.Messaging;
+using Azure.Messaging.EventGrid;
 using BlackJack.Events.Abstractions.Events;
 using BlackJack.Events.Abstractions.Sender;
 using Microsoft.Extensions.Logging;
@@ -14,17 +15,19 @@ public class EventGridSender: IEventGridSender
     public async Task<bool> SendEventAsync(IBlackJackEvent blackJackEvent)
     {
         _logger.LogInformation("Broadcasting event grid message {msg}", blackJackEvent);
-        var cloudEvent = new EventGridEvent(blackJackEvent.EventSource, blackJackEvent.EventType, blackJackEvent.Version, null);
+        var cloudEvent = new CloudEvent(blackJackEvent.EventSource, blackJackEvent.EventType, null);
         var response = await _client.SendEventAsync(cloudEvent);
         return !response.IsError;
     }
+
     public async Task<bool> SendEventAsync<TEventData>(IBlackJackEvent<TEventData> blackJackEvent)
     {
-        _logger.LogInformation("Broadcasting event grid message {msg}", JsonConvert.SerializeObject( blackJackEvent));
-        var cloudEvent = new EventGridEvent(blackJackEvent.EventSource, blackJackEvent.EventType, blackJackEvent.Version, BinaryData.FromObjectAsJson(blackJackEvent.Data));
-        cloudEvent.Data = BinaryData.FromObjectAsJson(blackJackEvent.Data);
+        _logger.LogInformation("Broadcasting event grid message {msg}", JsonConvert.SerializeObject(blackJackEvent));
+        var cloudEvent = new CloudEvent(
+            blackJackEvent.EventSource,
+            blackJackEvent.EventType,
+            blackJackEvent.Data);
         _logger.LogInformation("EventGridEvent with data {event}", JsonConvert.SerializeObject(cloudEvent));
-
         var response = await _client.SendEventAsync(cloudEvent);
         return !response.IsError;
     }
